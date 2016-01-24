763ab30605207eacc2f80908fb622fade3473870
Markus Török <markus.toeroek@gmail.com>
2016-01-24 13:49:18 +0100

    added a port variable

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 var pigLatin = require('./pig-latin.js');
+var port = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
   var message = pigLatin("california");
   res.send(message);
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
